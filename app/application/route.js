366f075cceb339197a6e3ccfa7ea8ff931aad3ee
Kara Fulgum <karafulgum@gmail.com>
2017-01-18 14:40:32 -0600

    Made hero reponsive to mobile

--- app/application/route.js
+++ app/application/route.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
-const api = '581a7fbf61a1f7b9'
+const api = '581a7fbf61a1f7b9';
 
 
 export default Ember.Route.extend({
   model() {
-    const url = `http://api.wunderground.com/api/${api}/conditions/q/CA/San_Francisco.json`;
+    const url = `http://api.wunderground.com/api/${api}/conditions/q/TN/Nashville.json`;
     return fetch(url)
       .then(res => res.json());
   }
