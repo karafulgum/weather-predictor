ccc9832ca92228a28f33bbe7417897ce17b269c5
Kara Fulgum <karafulgum@gmail.com>
2017-01-18 13:20:39 -0600

    Fetch API

--- app/application/route.js
+++ app/application/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+const api = '581a7fbf61a1f7b9'
+
 
 export default Ember.Route.extend({
+  model() {
+    const url = `http://api.wunderground.com/api/${api}/conditions/q/CA/San_Francisco.json`;
+    return fetch(url)
+      .then(res => res.json());
+  }
 });
