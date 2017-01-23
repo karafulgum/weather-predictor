e021cbec01d3d5d67357dfabf96cf06c791d8100
Kara Fulgum <karafulgum@gmail.com>
2017-01-23 16:50:51 -0600

    Add application controller

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,57 +1,11 @@
 import Ember from 'ember';
+const api = '581a7fbf61a1f7b9'
 
 export default Ember.Controller.extend({
-  // state= [
-  //   'Alabama',
-  //   'Alaska',
-  //   'Arizona',
-  //   'Arkansas',
-  //   'California',
-  //   'Colorado',
-  //   'Connecticut',
-  //   'Delaware',
-  //   'Florida',
-  //   'Georgia',
-  //   'Hawaii',
-  //   'Idaho',
-  //   'Illinois',
-  //   'Indiana',
-  //   'Iowa',
-  //   'Kansas',
-  //   'Kentucky',
-  //   'Louisiana',
-  //   'Maine',
-  //   'Maryland',
-  //   'Massachusetts',
-  //   'Michigan',
-  //   'Minnesota',
-  //   'Mississippi',
-  //   'Missouri',
-  //   'Montana',
-  //   'Nebraska',
-  //   'Nevada',
-  //   'New Hampshire',
-  //   'New Jersey',
-  //   'New Mexico',
-  //   'New York',
-  //   'North Carolina',
-  //   'North Dakota',
-  //   'Ohio',
-  //   'Oklahoma',
-  //   'Oregon',
-  //   'Pennsylvania',
-  //   'Rhode Island',
-  //   'South Carolina',
-  //   'South Dakota',
-  //   'Tennessee',
-  //   'Texas',
-  //   'Utah',
-  //   'Vermont',
-  //   'Virginia',
-  //   'Washington',
-  //   'West Virginia',
-  //   'Wisconsin',
-  //   'Wyoming',
-  // ],
-  // initial: '',
+  actions: {
+    searchLocation(state, city) {
+      fetch(`http://api.wunderground.com/api/${api}/conditions/q/${state}/${city}.json`)
+      .then((r => r.json()))
+    }
+  }
 });
