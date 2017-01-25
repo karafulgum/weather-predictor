4bd5980ce939ba75c44b8b24dfe4fe20fbc64354
Kara Fulgum <karafulgum@gmail.com>
2017-01-25 15:10:53 -0600

    Working on location search feature

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
-const api = '581a7fbf61a1f7b9'
+const api = '581a7fbf61a1f7b9';
 
 export default Ember.Controller.extend({
   actions: {
     searchLocation(state, city) {
       fetch(`http://api.wunderground.com/api/${api}/conditions/q/${state}/${city}.json`)
-      .then((r => r.json()))
+      .then((r => r.json()));
     }
   }
 });
