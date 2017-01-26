6e68c03efc652e56a99bb2478a007454069d822d
Kara Fulgum <karafulgum@gmail.com>
2017-01-26 10:43:20 -0600

    Made form and card mobile responsive

--- app/application/controller.js
+++ app/application/controller.js
@@ -5,8 +5,8 @@ export default Ember.Controller.extend({
   actions: {
     searchLocation(state, city) {
       fetch(`http://api.wunderground.com/api/${api}/conditions/q/${state}/${city}.json`)
-        .then((r => r.json()))
-        .then((m) => this.set('model', m));
+        .then(r => r.json())
+        .then((m) => this.set('model.conditions', m));
     }
   }
 });
--- app/application/route.js
+++ app/application/route.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
-const api = '581a7fbf61a1f7b9'
+const api = '581a7fbf61a1f7b9';
 
 
 export default Ember.Route.extend({
   model() {
-    const url = `http://api.wunderground.com/api/${api}/conditions/q/CA/San_Francisco.json`;
-    return fetch(url)
-      .then(res => res.json());
+    return Ember.RSVP.hash({
+      conditions: fetch(`http://api.wunderground.com/api/${api}/conditions/q/TN/Nashville.json`)
+        .then(res => res.json()),
+      forecast: fetch(`http://api.wunderground.com/api/${api}/forecast10day/q/TN/Nashville.json`)
+        .then(res => res.json()),
+    });
   }
 });
