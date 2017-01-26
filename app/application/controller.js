53458ae9aba26dcf71cd8b3fb228c3aea4631565
Kara Fulgum <karafulgum@gmail.com>
2017-01-26 16:23:13 -0600

    Update forecast for different locations

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 const api = '581a7fbf61a1f7b9';
 
 export default Ember.Controller.extend({
+  state: 'AL',
   actions: {
     searchLocation(state, city) {
-      fetch(`http://api.wunderground.com/api/${api}/conditions/q/${state}/${city}.json`)
-        .then(r => r.json())
-        .then((m) => this.set('model.conditions', m));
+      Ember.RSVP.hash({
+        conditions: fetch(`http://api.wunderground.com/api/${api}/conditions/q/${state}/${city}.json`)
+          .then(r => r.json()),
+        forecast: fetch(`http://api.wunderground.com/api/${api}/forecast/q/${state}/${city}.json`)
+          .then(r => r.json()),
+      }).then((m) => {
+        debugger;
+        this.set('model', m)
+      });
     }
   }
 });
